05b6b89fced6cd9714f574b839426e1f0547d645
Pascal Dihé <pascal@cismet.de>
2015-04-24 09:23:10 +0200

    #4  added debug messages, fixed general node trigger, attempt to fix scenario node trigger

--- src/main/java/de/cismet/cids/custom/crisma/trigger/WorldstateScenarioNodeTrigger.java
+++ src/main/java/de/cismet/cids/custom/crisma/trigger/WorldstateScenarioNodeTrigger.java
@@ -47,7 +47,7 @@ public class WorldstateScenarioNodeTrigger extends AbstractEntityCoreAwareCidsTr
 
     //~ Static fields/initializers ---------------------------------------------
 
-    private static final Logger LOGGER = Logger.getLogger(WorldstateNodeTrigger.class);
+    private static final Logger LOGGER = Logger.getLogger(WorldstateScenarioNodeTrigger.class);
     private static final ObjectMapper MAPPER = new ObjectMapper(new JsonFactory());
 
     //~ Instance fields --------------------------------------------------------
@@ -66,6 +66,11 @@ public class WorldstateScenarioNodeTrigger extends AbstractEntityCoreAwareCidsTr
         scenarioNodeBaseFolder = new File(Starter.FS_CIDS_DIR + File.separator
                         + RuntimeContainer.getServer().getDomainName()
                         + File.separator + "nodes/-1");
+        LOGGER.info("ScenarioNodeTrigger initialized with base folder: '"+scenarioNodeBaseFolder+"'");
+        if (!scenarioNodeBaseFolder.exists()) {
+            LOGGER.warn("base folder '"+scenarioNodeBaseFolder+"' does not exist, attempting to create it");
+            scenarioNodeBaseFolder.mkdirs();
+        }
     }
 
     //~ Methods ----------------------------------------------------------------
@@ -73,7 +78,7 @@ public class WorldstateScenarioNodeTrigger extends AbstractEntityCoreAwareCidsTr
     @Override
     public void beforeInsert(final String jsonObject, final User user) {
         if (LOGGER.isDebugEnabled()) {
-            LOGGER.debug("beforeInsert");
+            //LOGGER.debug("beforeInsert");
         }
     }
 
@@ -81,16 +86,14 @@ public class WorldstateScenarioNodeTrigger extends AbstractEntityCoreAwareCidsTr
     public void afterInsert(final String jsonObject, final User user) {
         try {
             final ObjectNode newWorldstate = (ObjectNode)MAPPER.reader().readTree(jsonObject);
-
-            if (!newWorldstate.hasNonNull("childworldstates")) {
+            LOGGER.info("WS["+newWorldstate.get("id").asText()+"] - attempting to create scenario node for WS '"+newWorldstate.get("name").asText()+"' or one of its children");
+            if (!newWorldstate.hasNonNull("childworldstates") 
+                    || !newWorldstate.get("childworldstates").isArray() 
+                    || ((ArrayNode)newWorldstate.get("childworldstates")).size() == 0) {
+                LOGGER.debug("WS["+newWorldstate.get("id").asText()+"] - WS is leaf -> create new scenario node");
                 createScenarioNode(user, newWorldstate);
             } else {
-                if (newWorldstate.get("childworldstates").isArray()) {
-                    final ArrayNode array = (ArrayNode)newWorldstate.get("childworldstates");
-                    if (array.size() > 0) {
-                        createScenarioNode(user, newWorldstate);
-                    }
-                }
+                 LOGGER.debug("WS["+newWorldstate.get("id").asText()+"] - WS is no leaf -> don't create new scenario node");
             }
         } catch (IOException ex) {
             LOGGER.error("can not parse json Object...", ex);
@@ -109,8 +112,13 @@ public class WorldstateScenarioNodeTrigger extends AbstractEntityCoreAwareCidsTr
         final String scenarioFileName = WorldstateNodeTriggerHelper.getNodeFileName(user, worldstate);
         final File scenarioFile = new File(scenarioNodeBaseFolder + File.separator + scenarioFileName + ".json");
         if (!scenarioFile.exists()) {
+            LOGGER.info("WS["+worldstate.get("id").asText()+"] - creating new scenario node for WS at '" +
+                    scenarioFile.getCanonicalPath()+"'");
             final Node scenarioNode = WorldstateNodeTriggerHelper.createScenarioNode(user, worldstate);
             MAPPER.writeValue(scenarioFile, scenarioNode);
+        } else {
+            LOGGER.debug("WS["+worldstate.get("id").asText()+"] - scenario node for WS already exists at '" +
+                    scenarioFile.getCanonicalPath()+"'");
         }
     }
 
@@ -122,9 +130,14 @@ public class WorldstateScenarioNodeTrigger extends AbstractEntityCoreAwareCidsTr
      */
     private void deleteScenarioNode(final User user, final ObjectNode worldstate) {
         final File scenarioNode = new File(scenarioNodeBaseFolder + File.separator
-                        + WorldstateNodeTriggerHelper.getNodeFileName(user, worldstate));
+                        + WorldstateNodeTriggerHelper.getNodeFileName(user, worldstate)+".json");
         if (scenarioNode.exists()) {
+            LOGGER.debug("WS["+worldstate.get("id").asText()+"] - deleting scenario node for WS at '" +
+                    scenarioNode+"'");
             scenarioNode.delete();
+        } else {
+            LOGGER.warn("WS["+worldstate.get("id").asText()+"] - cannot deleting scenario node for WS at '" +
+                    scenarioNode+"', node does not exist!");
         }
     }
 
@@ -167,18 +180,28 @@ public class WorldstateScenarioNodeTrigger extends AbstractEntityCoreAwareCidsTr
                             && (existingWorldstate.get("childworldstates").isArray()
                                 && (((ArrayNode)existingWorldstate.get("childworldstates")).size() > 0));
                 final File scenarioNode = new File(scenarioNodeBaseFolder + File.separator
-                                + WorldstateNodeTriggerHelper.getNodeFileName(user, newWorldstate));
+                                + WorldstateNodeTriggerHelper.getNodeFileName(user, newWorldstate) + ".json");
                 if (!existingHasChilds && newHasChilds) {
                     // we need to delete the scenario Node
+                    LOGGER.debug("WS["+id+"] - WS has new children -> delete existing scenario node");
                     deleteScenarioNode(user, newWorldstate);
                 } else if (existingHasChilds && !newHasChilds) {
                     // we need to create a scenario Node...
+                    LOGGER.debug("WS["+id+"] - WS children have been removed -> create new scenario node");
                     createScenarioNode(user, newWorldstate);
                 } else if (!newHasChilds && !scenarioNode.exists()) {
                     // this means that there should be a scenario node file but there isn't one
                     // just re create it
+                    LOGGER.debug("WS["+id+"] - scenario node does not exist -> create new scenario node");
                     createScenarioNode(user, newWorldstate);
+                } else {
+                    LOGGER.debug("WS["+id+"] new WS hasNewChildren: "+newHasChilds
+                            +", existing WS hasChildren: "+existingHasChilds 
+                            +", scenario node '"+scenarioNode+"' exists: '"+scenarioNode.exists() 
+                            +", dont create new scenario node");
                 }
+            } else {
+                LOGGER.warn("WS["+id+"] - WS does not exist, no update is performed (insert instead)");
             }
         } catch (IOException ex) {
             LOGGER.error("can not parse json object...", ex);
@@ -188,32 +211,51 @@ public class WorldstateScenarioNodeTrigger extends AbstractEntityCoreAwareCidsTr
     @Override
     public void afterUpdate(final String jsonObject, final User user) {
         if (LOGGER.isDebugEnabled()) {
-            LOGGER.debug("afterUpdate");
+            //LOGGER.debug("afterUpdate");
         }
         afterInsert(jsonObject, user);
     }
 
     @Override
-    public void beforeDelete(final String domain, final String classKey, final String objectId, final User user) {
+    public void afterDelete(final String domain, final String classKey, final String objectId, final User user) {
         if (LOGGER.isDebugEnabled()) {
-            LOGGER.debug("beforeDelete");
+            //LOGGER.debug("beforeDelete");
         }
     }
 
     @Override
-    public void afterDelete(final String domain, final String classKey, final String objectId, final User user) {
+    public void beforeDelete(final String domain, final String classKey, final String objectId, final User user) {
+        
+        String theClassKey = classKey;
+        
+        if(classKey.indexOf(domain) != 0) {
+            LOGGER.warn("WS["+objectId+"] - wrong class key '"+classKey+"' provided "
+                    + "in deleteObject, changing to '" + domain + "." + classKey+"'");
+            theClassKey = domain + "." + classKey;
+        }
+        
         final ObjectNode worldstate = this.getEntityCore()
-                    .getObject(user, classKey, objectId, "current", null, "1", null, null, null, true, true);
+                .getObject(
+                        user,
+                        theClassKey,
+                        objectId,
+                        "current",
+                        null,
+                        "1",
+                        null,
+                        "full",
+                        "default",
+                        true,
+                        true);
+        
         // check if the deleted worldstate is a scenario node...
-        if (!worldstate.hasNonNull("childworldsates")) {
+        if (!worldstate.hasNonNull("childworldstates") || 
+                (worldstate.get("childworldstates").isArray()
+                    && ((ArrayNode)worldstate.get("childworldstates")).size() == 0)) {
+            LOGGER.debug("WS["+objectId+"] - WS to be deleted is leaf -> remove the respective scenario node");
             deleteScenarioNode(user, worldstate);
         } else {
-            if (worldstate.get("childworldstates").isArray()) {
-                final ArrayNode array = (ArrayNode)worldstate.get("childworldstates");
-                if (array.size() > 0) {
-                    deleteScenarioNode(user, worldstate);
-                }
-            }
+            LOGGER.debug("WS["+objectId+"] - WS to be deleted is no leaf -> keep the respective scenario node");
         }
     }
 
